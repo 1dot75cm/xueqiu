186ff3f1f8665496269af70adf46e9d1d2ed33ae
mosquito <sensor.wen@gmail.com>
2019-01-24 16:29:03 +0800

    load cookie from file, browser or selenium
    Signed-off-by: mosquito <sensor.wen@gmail.com>

--- xueqiu/utils.py
+++ xueqiu/utils.py
@@ -16,9 +16,32 @@ from . import api
 from lxml import html
 from fake_useragent import UserAgent
 from selenium import webdriver
+import browsercookie
 import requests
 import os
 
+
+def get_cookies():
+    cj = requests.cookies.cookielib.LWPCookieJar(api.cookie_file)
+    cookies = [i for i in browsercookie.load() if i.domain.find("xueqiu")>0]
+    # load cookies from file
+    if os.path.exists(api.cookie_file):
+        cj.load(ignore_discard=True, ignore_expires=True)
+    # load cookies from browser
+    elif len(cookies) > 5:
+        [cj.set_cookie(ck) for ck in cookies]
+    # load cookies from selenium
+    else:
+        opts = webdriver.ChromeOptions()
+        opts._arguments = ['--disable-gpu', '--headless', '--remote-debugging-port=9090']
+        browser = webdriver.Chrome(options=opts)
+        browser.get(api.prefix)
+        for i in browser.get_cookies():
+            cj.set_cookie(requests.cookies.create_cookie(i['name'], i['value'],
+                domain=i.get('domain'), expires=i.get('expiry') or None,
+                rest={'HttpOnly':i.get('httpOnly')}, path='/'))
+        browser.close()
+    return cj
 
 def get_session():
     sess = requests.sessions.Session()
@@ -26,20 +49,8 @@ def get_session():
     sess.headers['Origin'] = api.prefix
     sess.headers['Referer'] = api.prefix
     sess.headers['User-Agent'] = UserAgent().random
-    # cookies
-    opts = webdriver.ChromeOptions()
-    opts._arguments = ['--disable-gpu', '--headless', '--remote-debugging-port=9090']
-    with webdriver.Chrome(options=opts) as browser:
-        browser.get(api.prefix)
-        #sess.cookies = requests.utils.cookiejar_from_dict(
-        #    {i['name']: i['value'] for i in browser.get_cookies()},
-        #    requests.cookies.cookielib.LWPCookieJar(api.cookie_file))
-        sess.cookies = requests.cookies.cookielib.LWPCookieJar(api.cookie_file)
-        for i in browser.get_cookies():
-            sess.cookies.set_cookie(
-                requests.cookies.create_cookie(i['name'], i['value'],
-                    domain=i.get('domain'), expires=i.get('expiry') or None,
-                    rest={'HttpOnly':i.get('httpOnly')}, path='/'))
+    # load cookie from file, browser or selenium
+    sess.cookies = get_cookies()
     # cookie directory
     if not os.path.exists(os.path.dirname(api.cookie_file)):
         os.mkdir(os.path.dirname(api.cookie_file))
